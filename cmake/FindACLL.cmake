3034448b6adec6febe1b7cc6889d8f6a1a2b31fb
Maximilian Voss <voss@adobe.com>
2021-02-06 20:10:08 +0100

    In which our hero updated the modules

--- cmake/FindACLL.cmake
+++ cmake/FindACLL.cmake
@@ -1,10 +1,27 @@
-find_path(ACLL_INCLUDE_DIR acll.h
+include(FindPackageHandleStandardArgs)
+
+find_path(ACLL_INCLUDE_DIR_INTERNAL acll.h
         HINTS
         include
         )
 
-FIND_LIBRARY(ACLL_LIBRARIES
+FIND_LIBRARY(ACLL_LIBRARIES_INTERNAL
         NAMES acll
         HINTS
         PATH_SUFFIXES lib64 lib
+        )
+mark_as_advanced(ACLL_LIBRARIES_INTERNAL)
+
+find_package_handle_standard_args(ACLL
+        REQUIRED_VARS ACLL_INCLUDE_DIR_INTERNAL ACLL_LIBRARIES_INTERNAL)
+
+find_package(CASSERTS REQUIRED)
+
+set(ACLL_INCLUDE_DIR
+        ${ACLL_INCLUDE_DIR_INTERNAL}
+        ${CASSERTS_INCLUDE_DIR}
+        )
+set(ACLL_LIBRARIES
+        ${ACLL_LIBRARIES_INTERNAL}
+        ${CASSERTS_LIBRARIES}
         )
